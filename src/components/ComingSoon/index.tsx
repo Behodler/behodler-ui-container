4e88d06bb34ce826301295154a97cba91f012eb5
Hieu Phan <hieutrunglephan@gmail.com>
2021-10-03 22:31:49 +0700

    integrate dapps nav with router

---
+++ src/components/ComingSoon/index.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react'
+
+const ComingSoon: FC = () => {
+    return (
+        <div className="w-screen h-full flex items-center justify-center">
+            <div className="text-3xl">Coming soon</div>
+        </div>
+    )
+}
+
+export default ComingSoon;
